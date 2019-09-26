a905c1cb27e8b3736f6d5810d40489be811fe3c9
prajjwaldangal <pdangal@uccs.edu>
2019-09-26 17:47:40 -0600

    added waiting times

--- binary-exponential-backoff.c
+++ binary-exponential-backoff.c
@@ -8,23 +8,33 @@
  #define N 2
 #endif
 
-struct Frame{
-	
-} Frame;
+#define TT 0.1 // transmission time = 0.1s
+
+// TO-DO: add frame model
 
 struct User
 {
-	int id;
-	double waiting_time;
+	// int id;
+	double waiting_time = 0.0;
 };
 
 typedef struct User UserType;
 
 int main() {
 	// compute average delay
-	UserType arr[N];
-	while (1) {
+	UserType user[N]; // N number of users
+	double time_spent = 0.0;
+	clock_t start, end;
 
+	int turn;
+	while (1) {
+		turn = (int) rand() % 2;// 
+		start = clock();
+		if (turn==0) {
+			user[1].waiting_time += (double) (clock()-start)/CLOCK_PER_SEC;
+		} else {
+			user[0].waiting_time += (double) (clock()-start)/CLOCK_PER_SEC;
+		}
 	}
 	return 0;
 }
