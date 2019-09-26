ad22a6c08acb922fcebcf3fcbe6e275005e18575
prajjwaldangal <pdangal@uccs.edu>
2019-09-26 17:24:25 -0600

    added partial skeleton

---
+++ binary-exponential-backoff.c
@@ -0,0 +1,30 @@
+// beb
+#include <stdio.h>
+#include <time.h>
+#include <unistd.h>
+// two computers
+
+#ifndef N
+ #define N 2
+#endif
+
+struct Frame{
+	
+} Frame;
+
+struct User
+{
+	int id;
+	double waiting_time;
+};
+
+typedef struct User UserType;
+
+int main() {
+	// compute average delay
+	UserType arr[N];
+	while (1) {
+
+	}
+	return 0;
+}
